810dcc45b43c0e89ef6363f666b3cea7e39be0c9
Sidhant khosla <sid@Sidhants-MacBook-Pro.local>
2020-07-27 22:24:31 +0100

    removing trail auth codes

--- sms.py
+++ sms.py
@@ -1,8 +1,8 @@
 # Must install twilio using pip
 from twilio.rest import Client 
  
-account_sid = 'AC84246579081b286453be3511e6016f25' 
-auth_token = '430803ce6bd1ddfec945a571d992d605' 
+account_sid = 'Get using Twilio website'
+auth_token = 'Get using Twilio website' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
